4941634b0355c75f9ada59421ea64dc6db9782b7
John Lamar <okaforjohnekene316@gmail.com>
2025-01-05 13:51:29 +0000

    added timer

--- client/src/app/(dashboard)/dashboard/page.tsx
+++ client/src/app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,51 @@
 "use client";
 
 import Header from "@/components/Header";
+import { useState, useEffect } from "react";
 import { useUser } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
 
 const Dashboard = () => {
   const { user } = useUser();
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
   const fullName = user?.fullName ?? "User";
+
+  useEffect(() => {
+    const updateClockAndDate = () => {
+      const now = new Date();
+      const formattedTime = now.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+      setTime(formattedTime);
+
+      const formattedDate = now.toLocaleDateString([], {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+      setDate(formattedDate);
+    };
+
+    const timerId = setInterval(updateClockAndDate, 1000);
+    updateClockAndDate();
+
+    return () => clearInterval(timerId);
+  }, []);
 
   return (
     <>
       <Header title="Dashboard" subtitle="View your Dashboard" />
-      <div className="min-h-screen">
+      <div className="min-h-screen relative">
+        <div className="absolute top-4 right-4 bg-gray-800 text-white-50 px-4 py-2 rounded-lg shadow-md text-sm font-mono z-10">
+          <div>{time}</div>
+          <div className="text-xs text-gray-300">{date}</div>
+        </div>
+
         <div className="flex flex-col sm:flex-row items-center justify-center mb-8 bg-customgreys-secondarybg p-10 rounded">
           <div className="flex-1 text-center sm:text-left mb-6 sm:mb-0">
             <h1 className="text-2xl font-bold text-customgreys-primarybg">
